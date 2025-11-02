64b14240e611526f44fe66b4ea35d024cd75d591
MadanBelbase <madanbelbase927@gmail.com>
2025-11-02 22:34:22 +0545

    day 1

---
+++ Phase 1: Foundations Python Basics/Day 1/basic.py
@@ -0,0 +1,29 @@
+x = "body"
+print(x)
+type(x)
+
+y = 5
+print(y)
+type(y)
+
+z = 3.14
+print(z)
+type(z)
+a = True
+print(a)
+type(a)
+b = None
+print(b)
+type(b)
+c = [1, 2, 3, 4, 5]
+print(c)
+type(c)
+d = (1, 2, 3, 4, 5)
+print(d)
+type(d)
+e = { "name": "John", "age": 30 }
+print(e)
+type(e)
+f = {1, 2, 3, 4, 5}
+print(f)
+type(f)
---
+++ Phase 1: Foundations Python Basics/Day 1/list.py
@@ -0,0 +1,4 @@
+ // list in python  
+
+# A list is a collection which is ordered and changeable. In Python, lists are written with square brackets.
+
