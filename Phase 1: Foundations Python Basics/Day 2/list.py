46284748ca6ec58317915ab6a5deb1d91b9070e7
MadanBelbase <madanbelbase927@gmail.com>
2025-11-03 22:04:00 +0545

    day 2

---
+++ Phase 1: Foundations Python Basics/Day 2/list.py
@@ -0,0 +1,14 @@
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])  # Output: 10
+
+print(my_list[2])  # Output: 30
+print(my_list[-1]) # Output: 50
+my_list[1] = 25
+print(my_list)     # Output: [10, 25, 30, 40, 50]
+
+# list slicing 
+print(my_list[1:4])  # Output: [25, 30, 40]
+print(my_list[:3])   # Output: [10, 25, 30]
+
+# manupulation of list
+
