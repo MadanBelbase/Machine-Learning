26b7e19b7897e20c4bb9a63b3d341e67dd823711
MadanBelbase <madanbelbase927@gmail.com>
2025-12-13 21:35:12 +0545

    adding one step

---
+++ Phase 2: Machine Learning Basics/2 Practical  ML/2 Toy Project/Project_Explain.py
@@ -0,0 +1,7 @@
+# in this project  i am going use google colab to explain a toy machine learning project using a simple dataset and model.
+#  data  is placement.csv
+
+# logistic regression model to predict whether a student will be placed or not based on their cgpa and iq score.
+# how logicstic regression works in this project.
+
+
